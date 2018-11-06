9ec82df88def2a1995b191e9f942a1038439fba8
Ansel Bridgewater <iamansel@gmail.com>
2018-11-06 15:16:04 -0800

    Added some validation for your backpack model

--- learning_models/app/models/backpack.rb
+++ learning_models/app/models/backpack.rb
@@ -1,2 +1,18 @@
 class Backpack < ApplicationRecord
+  validates :color, presence: true
+  validates :owner_name, presence: true
+  validates :num_pens, length: {
+    minimum: 0,
+    maximum: 100000,
+    wrong_length: 'Invalid number of pens',
+    too_long: "%{count} is too many pens. There is no way you fit that many pens into your backpack.",
+    too_short: "You cannot have a negative number of pens."
+  }
+  validates :owner_name, length: {
+    minimum: 0,
+    maximum: 1000,
+    wrong_length: 'Invalid length',
+    too_long: "%{count} charters is the maximum allowed.",
+    too_short: "You must have at least %{count} characters."
+  }
 end
